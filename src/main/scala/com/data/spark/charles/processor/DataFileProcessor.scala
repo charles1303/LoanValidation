4a805d0546f692c5cd0be3c8f5296d0eb5d060c3
Charles_Uye <charles.uye@konga.com>
2016-07-04 11:38:32 +0100

    Let Flume Handle File Upload to HDFS

--- src/main/scala/com/data/spark/charles/processor/DataFileProcessor.scala
+++ src/main/scala/com/data/spark/charles/processor/DataFileProcessor.scala
@@ -8,19 +8,28 @@ import scala.util._
 import org.apache.spark.rdd.RDD
 import org.apache.hadoop.conf._
 import org.apache.hadoop.fs._
+import com.data.spark.charles.SendOutputStream
 
 class DataFileProcessor extends Serializable{
-    
+    val hdfsBaseUrl = Properties.envOrElse("HDFS_BASE_URL", "hdfs://127.0.0.1:9000")
+      val dataSourceBaseUrl = Properties.envOrElse("DATA_SOURCE_BASE_URL", "file:///home")
   def updateModel(lineStream: RDD[String])={
     lineStream.foreach { fileName =>
-      val hdfsBaseUrl = Properties.envOrElse("HDFS_BASE_URL", "hdfs://127.0.0.1:9000")
-      val dataSourceBaseUrl = Properties.envOrElse("DATA_SOURCE_BASE_URL", "file:///home")
+      
       
       val inputFileData = RequestInputStream.getStreamingContext().sparkContext.textFile(dataSourceBaseUrl+"/uploads/"+fileName).cache()
       val randomForestModel = new RandomForestImplClass(inputFileData)
       randomForestModel.updateTrainingModel(inputFileData)
-      
-      val conf = new Configuration();
+      //val sos = new SendOutputStream
+      //sos.
+      //Let Flume and its Interceptor handle this
+      //deleteHadoopFile(fileName,inputFileData)
+        
+      }
+  }
+  
+  def deleteHadoopFile(fileName: String,inputFileData: RDD[String]) = {
+    val conf = new Configuration();
       conf.set("fs.hdfs.impl",(classOf[org.apache.hadoop.hdfs.DistributedFileSystem]).getName);
       conf.set("fs.file.impl",(classOf[org.apache.hadoop.fs.LocalFileSystem]).getName);
       val  hdfs = FileSystem.get(java.net.URI.create(hdfsBaseUrl), conf);
@@ -30,8 +39,6 @@ class DataFileProcessor extends Serializable{
           } catch { 
                 case _ : Throwable => { } 
           }
-        
-      }
   }
   
   
